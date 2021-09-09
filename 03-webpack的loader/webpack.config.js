cb8806fa45c0cea1df87d21d4b28a1e5de0e8665
sunao <876986155@qq.com>
2021-09-09 22:21:28 +0800

    图片打包   webpack5的配置方法

--- 03-webpack的loader/webpack.config.js
+++ 03-webpack的loader/webpack.config.js
@@ -1,10 +1,20 @@
+const path = require('path');
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-  };
+  module: {
+    rules: [{
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.less$/i,
+      //   loader: [
+      //     // compiles Less to CSS
+      //     'style-loader',
+      //     'css-loader',
+      //     'less-loader'
+      //   ],
+      // },
+      {test:/\.(jpg|png|gif)$/,type: 'asset/resource'}
+    ],
+  },
+};
